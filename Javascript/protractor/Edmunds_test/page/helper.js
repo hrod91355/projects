1819b2cb4dd9cf960ab83c2ada14ebbb855c020f
hrod91355 <rodarte.hector@gmail.com>
2018-01-22 16:44:04 -0700

    added New Car nav menu test

--- Javascript/protractor/Edmunds_test/page/helper.js
+++ Javascript/protractor/Edmunds_test/page/helper.js
@@ -6,21 +6,20 @@ module.exports = {
             return Math.floor(Math.random() * numberOfItems);
         }).then(function(randomNumber) {
             allOptions.get(randomNumber).getText().then(function(text) {
-              global.globalDropDownSelection = text;
+                globalDropDownSelection = text;
+                console.log(globalDropDownSelection);
             });
             allOptions.get(randomNumber).click();
             browser.sleep(1000);
         });
     },
 
-    titleValidation: function(globalDropDownSelection ) {
-        expect(browser.getTitle()).toContain(globalDropDownSelection );
+    selectFromList: function() {
+
+    },
+
+    titleValidation: function(globalDropDownSelection) {
+        expect(browser.getTitle()).toContain(globalDropDownSelection);
     }
 
 };
-
-
-
-// browser.globalDropDownSelection =allOptions.get(randomNumber).getText().then(function(text) {
-//                 return  = text;
-//};
