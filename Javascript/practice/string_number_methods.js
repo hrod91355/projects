76fa76da5914c9241da17745a9eceb3e98957d8a
hrod91355 <rodarte.hector@gmail.com>
2018-01-25 19:16:02 -0700

    javascript practice code

---
+++ Javascript/practice/string_number_methods.js
@@ -0,0 +1,23 @@
+
+
+let theString = "Hello this is a string for use"
+
+
+
+// startsWith()
+// endsWith()
+// includes()
+
+
+// console.log(theString.startsWith('Hello'));
+// console.log(theString.endsWith('use'));
+// console.log(theString.includes('use'));
+
+
+
+// number methods
+
+console.log(Number.isFinite(3));
+
+console.log(Number.isNaN(3));
+console.log(Number.integer(3));
