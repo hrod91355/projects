4e6a25ba34364e21c85c0ab7028e56e24ab637d9
hrod91355 <rodarte.hector@gmail.com>
2018-02-06 09:39:17 -0700

    updated db test feature context file

--- PHP/DB_testing/features/bootstrap/FeatureContext.php
+++ PHP/DB_testing/features/bootstrap/FeatureContext.php
@@ -20,28 +20,5 @@ class FeatureContext extends User implements Context
     public function __construct()
     {
     }
-
-    /**
-     * @Given I am DBA connected to the DB
-     */
-    public function iAmDbaConnectedToTheDb()
-    {
-        $this->getUsersLastNames();
-    }
-
-    /**
-     * @When New user info is added to the db
-     */
-    public function newUserInfoIsAddedToTheDb()
-    {
-        $this->getUsersLastNames();
-    }
-
-    /**
-     * @Then I should see the users info in the DB
-     */
-    public function iShouldSeeTheUsersInfoInTheDb()
-    {
-        throw new PendingException();
-    }
+    
 }
